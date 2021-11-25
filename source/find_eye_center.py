28f8dd56b5bde2b01adb6a39846b0fcd3875a337
hussein987 <husseinyounes2015@gmail.com>
2021-11-25 21:39:19 +0300

    bug fixes

--- source/find_eye_center.py
+++ source/find_eye_center.py
@@ -1,15 +1,11 @@
 import cv2
-
-
 import math
-
-import cv2
 import numpy as np
 
+def get_eye_pupil(img_file):
+    img = cv2.imread(img_file)
+    scaling_factor = 0.5
 
-def main():
-    img = cv2.imread('dataset/UnityEyes/imgs/1.jpg')
-    scaling_factor = 0.7
 
     img = cv2.resize(img, None, fx=scaling_factor, fy=scaling_factor, interpolation=cv2.INTER_AREA)
     cv2.imshow('Input', img)
@@ -23,18 +19,28 @@ def main():
         rect = cv2.boundingRect(contour)
         x, y, width, height = rect
         radius = 0.25 * (width + height)
+        print(img.shape, width, height, area)
 
-        area_condition = (100 <= area <= 200)
+        area_condition = (100 <= area <= 225)
         symmetry_condition = (abs(1 - float(width)/float(height)) <= 0.2)
         fill_condition = (abs(1 - (area / (math.pi * math.pow(radius, 2.0)))) <= 0.3)
 
+        print("Ckecking...")
         if area_condition and symmetry_condition and fill_condition:
-            cv2.circle(img, (int(x + radius), int(y + radius)), int(1.3*radius), (0,180,0), -1, color='r')
+            print("HERE")
+            print(int(x + radius), int(y + radius))
+            cv2.circle(img, (int(x + radius), int(y + radius)), int(radius), color=(255, 0, 0))
+            cv2.circle(img, (int(x + radius), int(y + radius)), 0, color=(0, 0, 255))
 
+    cv2.namedWindow('Pupil Detector', cv2.WINDOW_KEEPRATIO)
     cv2.imshow('Pupil Detector', img)
 
     c = cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+def main():
+    get_eye_pupil('dataset/UnityEyes/imgs/1.jpg')
+
 
 if __name__ == "__main__":
     main()
